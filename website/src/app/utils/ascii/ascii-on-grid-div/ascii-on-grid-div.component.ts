b2c2e9a72344d91aa9e50864dcb88fae50cd7534
sarah buisson <sarahbuisson@gmail.com>
2020-07-04 20:35:22 +0200

    better object placement in zone

--- website/src/app/utils/ascii/ascii-on-grid-div/ascii-on-grid-div.component.ts
+++ website/src/app/utils/ascii/ascii-on-grid-div/ascii-on-grid-div.component.ts
@@ -15,26 +15,37 @@ import {
 })
 export class AsciiOnGridDivComponent implements OnInit, AfterViewInit, AfterViewChecked  {
   @HostBinding('style.grid-column')
-  public xRepeat: string;
+  public gridColumn: string;
 
   @HostBinding('style.grid-row')
-  public yRepeat: string;
+  public gridRow: string;
+
+  public xRepeat: number;
+
+  public yRepeat: number;
 
 
 
   @ViewChild('contentDiv') contentDiv: ElementRef
 
-  constructor(private readonly _changeDetectorRef: ChangeDetectorRef) { }
+  constructor(private readonly _changeDetectorRef: ChangeDetectorRef, private host: ElementRef,
+              private readonly renderer:Renderer2) { }
 
   ngOnInit(): void {
   }
   tick_then(fn: () => any) { setTimeout(fn, 0); }
 
+
   ngAfterViewInit() {
     if(this.contentDiv) {
-       this.tick_then( ()=>{
-        this.yRepeat = 'span '+Math.ceil((<HTMLElement>this.contentDiv.nativeElement).getBoundingClientRect().height / 15.1)
-        this.xRepeat = 'span '+Math.ceil((<HTMLElement>this.contentDiv.nativeElement).getBoundingClientRect().width / 7.82)
+      this.yRepeat = Math.ceil((<HTMLElement>this.contentDiv.nativeElement).getBoundingClientRect().height / 15.1)
+      this.xRepeat = Math.ceil((<HTMLElement>this.contentDiv.nativeElement).getBoundingClientRect().width / 7.82)
+
+      this.renderer.setStyle(this.host.nativeElement,'grid-column', 'span ' + this.xRepeat);
+      this.renderer.setStyle(this.host.nativeElement,'grid-row', 'span ' + this.yRepeat);
+      this.tick_then(() => {
+        this.gridColumn = 'span ' + this.xRepeat;
+        this.gridRow = 'span ' + this.yRepeat;
         this._changeDetectorRef.detectChanges();
       })
 
