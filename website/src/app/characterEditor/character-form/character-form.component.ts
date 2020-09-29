2dda25b6703b50b0121892fa933f72359ecef97c
sarah buisson <sarahbuisson@gmail.com>
2020-09-30 00:34:04 +0200

    rotating render in new form

--- website/src/app/characterEditor/character-form/character-form.component.ts
+++ website/src/app/characterEditor/character-form/character-form.component.ts
@@ -1,8 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {CharacterRenderService, CharacterRenderData} from "../character-render.service";
 import {GenerateLabService} from "../../labyrinth/service/generate-lab.service";
 import {DataStorageService} from "../../labyrinth/service/data-storage.service";
-
 
 
 
@@ -11,32 +10,53 @@ import {DataStorageService} from "../../labyrinth/service/data-storage.service";
   templateUrl: './character-form.component.html',
   styleUrls: ['./character-form.component.css']
 })
-export class CharacterFormComponent implements OnInit {
+export class CharacterFormComponent implements OnInit, OnDestroy {
 
 
   data: CharacterRenderData = new CharacterRenderData()
 
   render: String = ""
-  renderReverse: String = ""
-  renderFront: String = ""
-  renderBack: String = ""
+  private interval:any;
+  private time: number = 0;
+  private direction: string;
+
 
   constructor(public renderService: CharacterRenderService,
-              private dataStorage:DataStorageService) {
+              private dataStorage: DataStorageService) {
 
   }
 
   update() {
-    this.render = this.renderService.render(this.data)
-    this.renderReverse = this.renderService.render(this.data, 'RIGHT')
-    this.renderFront = this.renderService.render(this.data, 'BOTTOM')
-    this.renderBack = this.renderService.render(this.data, 'TOP')
+    this.render = this.renderService.render(this.data, this.direction)
   }
 
 
   ngOnInit(): void {
     this.update()
     this.dataStorage.saveCharacter(this.data)
+
+    this.interval = setInterval(() => {
+      this.time = (this.time + 1) % 4;
+      switch (this.time) {
+        case 0:
+          this.direction = 'LEFT';
+          break;
+        case 1:
+          this.direction = 'BOTTOM';
+          break;
+        case 2:
+          this.direction = 'RIGHT';
+          break;
+        case 3:
+          this.direction = 'TOP';
+          break;
+      }
+      this.render = this.renderService.render(this.data, this.direction);
+    }, 1000)
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
   }
 
 }
