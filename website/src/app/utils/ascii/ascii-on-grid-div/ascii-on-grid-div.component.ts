b1e8ac830fb2f474eca46ffa8fa3f500e0e25e66
sarah buisson <sarahbuisson@gmail.com>
2020-10-03 23:51:03 +0200

    various fix

--- website/src/app/utils/ascii/ascii-on-grid-div/ascii-on-grid-div.component.ts
+++ website/src/app/utils/ascii/ascii-on-grid-div/ascii-on-grid-div.component.ts
@@ -1,60 +1,32 @@
 import {
-  AfterViewChecked,
-  AfterViewInit,
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
   Component, ElementRef, HostBinding, Input,
-  OnInit, Renderer2, ViewChild
+  OnInit, ViewChild
 } from '@angular/core';
 
 @Component({
   selector: 'app-ascii-on-grid-div',
-  templateUrl: './ascii-on-grid-div.component.html',
-  styleUrls: ['./ascii-on-grid-div.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  template: '<ng-content></ng-content>',
+  styleUrls: ['./ascii-on-grid-div.component.css']
 })
-export class AsciiOnGridDivComponent implements OnInit, AfterViewInit, AfterViewChecked  {
+export class AsciiOnGridDivComponent implements OnInit {
   @HostBinding('style.grid-column')
   public gridColumn: string;
 
   @HostBinding('style.grid-row')
   public gridRow: string;
+  @Input()
+  public width: number = 1;
 
-  public xRepeat: number;
+  @Input()
+  public height: number | undefined;
 
-  public yRepeat: number;
-
-
-
-  @ViewChild('contentDiv') contentDiv: ElementRef
-
-  constructor(private readonly _changeDetectorRef: ChangeDetectorRef, private host: ElementRef,
-              private readonly renderer:Renderer2) { }
+  constructor() { }
 
   ngOnInit(): void {
-  }
-  tick_then(fn: () => any) { setTimeout(fn, 0); }
+    this.gridColumn = 'span ' + this.width;
 
-
-  ngAfterViewInit() {
-    if(this.contentDiv) {
-      const old={ yRepeat: this.yRepeat,xRepeat: this.xRepeat,}
-
-      this.yRepeat = Math.ceil((<HTMLElement>this.contentDiv.nativeElement).getBoundingClientRect().height / 15.1)
-      this.xRepeat = Math.ceil((<HTMLElement>this.contentDiv.nativeElement).getBoundingClientRect().width / 7.82)
-      if (this.xRepeat != old.xRepeat || this.yRepeat != old.yRepeat) {
-        this.renderer.setStyle(this.host.nativeElement, 'grid-column', 'span ' + this.xRepeat);
-        this.renderer.setStyle(this.host.nativeElement, 'grid-row', 'span ' + this.yRepeat);
-        this.tick_then(() => {
-          this.gridColumn = 'span ' + this.xRepeat;
-          this.gridRow = 'span ' + this.yRepeat;
-          this._changeDetectorRef.detectChanges();
-        })
-      }
-    }
+    if(this.height)
+      this.gridRow = 'span ' + this.height;
   }
 
-  ngAfterViewChecked(): void {
-    this.ngAfterViewInit();
-  }
 }
